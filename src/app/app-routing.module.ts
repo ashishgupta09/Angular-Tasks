e95b78ae480cf4c75404897b3f0a4b2111bd6b7b
Ashish Gupta <115079049+ashishgupta09@users.noreply.github.com>
2024-06-01 14:29:09 +0530

    drag-drop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrudOpertionComponent } from './pages/crud-opertion/crud-opertion.component';
+import { DragDropComponent } from './pages/drag-drop/drag-drop.component';
 
 const routes: Routes = [
-  { path: 'crud', component: CrudOpertionComponent }
+  { path: 'crud', component: CrudOpertionComponent },
+  { path: 'drag', component: DragDropComponent }
 ];
 
 @NgModule({
--- src/app/pages/drag-drop/drag-drop.component.ts
+++ src/app/pages/drag-drop/drag-drop.component.ts
@@ -7,4 +7,80 @@ import { Component } from '@angular/core';
 })
 export class DragDropComponent {
 
+  tickerArray: any[] = [
+    {
+      ticketId: 'Jira-001',
+      ticketName: 'Layout Page',
+      status: 'In Progress'
+    },
+    {
+      ticketId: 'Jira-004',
+      ticketName: 'Password',
+      status: 'Done'
+    },
+    {
+      ticketId: 'Jira-002',
+      ticketName: 'Ionic',
+      status: 'To Do'
+    },
+    {
+      ticketId: 'Jira-003',
+      ticketName: 'React',
+      status: 'In Progress'
+    },
+    {
+      ticketId: 'Jira-005',
+      ticketName: 'Single Page',
+      status: 'Done'
+    },
+    {
+      ticketId: 'Jira-006',
+      ticketName: 'Star',
+      status: 'To Do'
+    },
+    {
+      ticketId: 'Jira-007',
+      ticketName: 'News',
+      status: 'In Progress'
+    },
+    {
+      ticketId: 'Jira-008',
+      ticketName: 'Stucture',
+      status: 'Done'
+    },
+    {
+      ticketId: 'Jira-009',
+      ticketName: 'Login-page',
+      status: 'To Do'
+    },
+    {
+      ticketId: 'Jira-010',
+      ticketName: 'homepage Page',
+      status: 'In Progress'
+    }
+  ]
+  currentItem: any;
+
+  filterTicker(status: string) {
+    return this.tickerArray.filter(m => m.status == status);
+  }
+
+  onDrop(event: any, status: string) {
+    event.preventDefault();
+    const record = this.tickerArray.find(m => m.ticketId == this.currentItem.ticketId)
+    if (record != undefined) {
+      record.status = status;
+    }
+
+    this.currentItem = null;
+  }
+
+  onDragStart(item: any) {
+    this.currentItem = item;
+  }
+
+  onDragOver(event: any) {
+    event.preventDefault();
+  }
 }
+
